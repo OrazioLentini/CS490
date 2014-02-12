c805cd7b88112035deebc4e86877d0851d872240
Nicole <ijuchen32@gmail.com>
2014-02-12 11:37:28 -0500

    Create connect.php

---
+++ Front/connect.php
@@ -0,0 +1,13 @@
+<?php
+	$Username=$_POST['name'];
+	$Password=$_POST['word'];
+	$db = curl_init();
+	curl_setopt($db, CURLOPT_URL, "http://web.njit.edu/~rab25");
+	 curl_setopt($db, CURLOPT_POST, 1);
+    //curl_setopt($db, CURLOPT_POSTFIELDS, $fields);
+    curl_setopt($db, CURLOPT_POSTFIELDS, "Username=$Username&Password=$Password&Check=$check");
+    curl_setopt($db, CURLOPT_FOLLOWLOCATION, 1);
+    
+    $outputDB = curl_exec($db);
+    curl_close($db);
+?>
