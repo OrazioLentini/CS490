f30210d77c51b0ac1b055badc0ba326c1318415d
Orazio Lentini <oraziio@hotmail.com>
2014-02-12 15:55:23 -0500

    Update connect.php

--- Front/connect.php
+++ Front/connect.php
@@ -1,13 +1,14 @@
 <?php
 	$Username=$_POST['name'];
 	$Password=$_POST['word'];
-	$db = curl_init();
-	curl_setopt($db, CURLOPT_URL, "http://web.njit.edu/~rab25");
-	 curl_setopt($db, CURLOPT_POST, 1);
-    //curl_setopt($db, CURLOPT_POSTFIELDS, $fields);
-    curl_setopt($db, CURLOPT_POSTFIELDS, "Username=$Username&Password=$Password");
-    curl_setopt($db, CURLOPT_FOLLOWLOCATION, 1);
+	
+	$crl = curl_init();
+	curl_setopt($crl, CURLOPT_URL, "http://web.njit.edu/~rab25");
+	curl_setopt($crl, CURLOPT_POST, 1);
+    	//curl_setopt($crl, CURLOPT_POSTFIELDS, $fields);
+    	curl_setopt($crl, CURLOPT_POSTFIELDS, "Username=$Username&Password=$Password");
+	curl_setopt($crl, CURLOPT_FOLLOWLOCATION, 1);
     
-    $outputDB = curl_exec($db);
-    curl_close($db);
+	$crlm = curl_exec($crl);
+    	curl_close($crl);
 ?>
