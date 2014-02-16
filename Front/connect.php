9b5b92c03d496e76196cf89580551b241f678a0a
Nicole <ijuchen32@gmail.com>
2014-02-16 00:01:27 -0500

    Update connect.php

--- Front/connect.php
+++ Front/connect.php
@@ -1,14 +1,14 @@
 <?php
-	$Username=$_POST['name'];
-	$Password=$_POST['word'];
-	
-	$crl = curl_init();
-	curl_setopt($crl, CURLOPT_URL, "http://web.njit.edu/~rab25/CS490/Middle/middlelogin.php");
-	curl_setopt($crl, CURLOPT_POST, 1);
-    	//curl_setopt($crl, CURLOPT_POSTFIELDS, $fields);
-    	curl_setopt($crl, CURLOPT_POSTFIELDS, "Username=$Username&Password=$Password");
-	curl_setopt($crl, CURLOPT_FOLLOWLOCATION, 1);
-    
-	$crlm = curl_exec($crl);
-    	curl_close($crl);
+        $Username=$_POST['Username'];
+        $Password=$_POST['Password'];
+
+        $crl =curl_init();
+        curl_setopt($crl, CURLOPT_URL,
+        "http://web.njit.edu/~rab25/CS490/Middle/middlelogin.php");
+        curl_setopt($crl, CURLOPT_POST, 1);
+        curl_setopt($crl, CURLOPT_POSTFIELDS, "Username=$Username&Password=$Password");
+        curl_setopt($crl, CURLOPT_FOLLOWLOCATION, 1);
+        $crlm = curl_exec($crl);
+        curl_close($crl);
+
 ?>
