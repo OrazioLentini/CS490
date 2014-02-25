aacbf7f0192dc33485fd1bcbe0fd176a7846356d
OrazioLentini <oraziio@hotmail.com>
2014-02-25 16:34:48 -0500

    Update middlelogin.php

--- Middle/middlelogin.php
+++ Middle/middlelogin.php
@@ -3,7 +3,7 @@
     $Password = $_POST['Password'];
     
     $url = "https://moodleauth00.njit.edu/cpip_serv/login.aspx?esname=moodle";
-    $postfields = "__VIEWSTATE=%2FwEPDwUJNDIzOTY1MjU5ZGQdLVY%2B81xpmN0ATE7y41EHAhVaCA%3D%3D&txtUCID=".$Username."&txtPasswd=".$Password."&btnLogin=Login&__EVENTVALIDATION=%2FwEWBAK7zbGBDQLr9O%2BIBwK01ba%2BBAKC3IeGDOn1GTxupWw9xfJhOXrBSFX6INdC";        
+    $postfields = "__VIEWSTATE=%2FwEPDwUJNDIzOTY1MjU5ZGQdLVY%2B81xpmN0ATE7y41EHAhVaCA%3D%3D&txtUCID=".$Username."&txtPasswd=".$Password."&btnLogin=Login";        
     
     // BOUNCE TO NJIT TO CHECK STATUS
     $bounce = curl_init();
@@ -17,7 +17,6 @@
     curl_close($bounce);
 
     // SENDS POST TO BACKEND TO CHECK DATABASE
-    $fields = array('Username' => $Username, 'Password' => $Password, 'Check' => $Check);
 
     $db = curl_init();
     curl_setopt($db, CURLOPT_URL, "http://web.njit.edu/~ovl2/CS490/Back/backlogin.php");
