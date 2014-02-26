f158752914aa48066b0595db302f70650b539df4
OrazioLentini <oraziio@hotmail.com>
2014-02-26 13:44:32 -0500

    Update backlogin.php

--- Back/backlogin.php
+++ Back/backlogin.php
@@ -1,39 +1,55 @@
 <?php
-    $con = mysql_connect("sql.njit.edu","ovl2_proj","EWzpulq1");
+    $con = mysql_connect("sql.njit.edu","ovl2_proj","cs490");
     if(!$con) {
         die('Could not connect: ' . mysql_error());
     }
 
     mysql_select_db("ovl2_proj", $con);
    
-    $User = $_POST['Username'];
-    $Pass = $_POST['Password'];
-    $Bounce = $_POST['Check'];
+    $request = file_get_contents('php://input');
+    $recieve = json_decode($request);
     
-    $sql = mysql_query("SELECT COUNT(*) FROM Login L WHERE Username = '$User' AND Password = '$Pass'");
-    $info = mysql_fetch_assoc($sql);
-    $ver = $info['COUNT(*)'];
-    
-    //echo $ver;
-    if($Bounce == 302){
-        $njit = 'SUCCESSFUL';
-    }
-    else{
-        $njit = 'UNSUCCESSFUL';
-    }
-    
-    if($ver == 1){
-        $db = 'SUCCESSFUL';
-    }
-    else{
-        $db = 'UNSUCCESSFUL';
-    }
-    
-    printf ("NJIT Login: ");
-    echo "<b>$njit</b>";
-    echo "<br>";
-    printf ("Project Login: ");
-    echo "<b>$db</b>";
-   
+    $User = $recieve->Username;
+    $Pass = $recieve->Password;
+    $Check = $recieve->Check;
 
+   if ($Check == 'teacher') {
+        
+        $sql = mysql_query("SELECT COUNT(*) FROM TeacherLogin TL WHERE Teacher_ID = '$User' AND Teacher_PW = '$Pass'");
+        $info = mysql_fetch_assoc($sql);
+        $ver = $info['COUNT(*)'];
+        
+        if ($ver == 1) {
+            session_start();
+            $_SESSION['UserTC'] = $User;
+            echo "teacher connected";
+        }
+        else {
+            header ("Location: http://web.njit.edu/~ovl2/CS490/Front/login.html");
+            echo "Invalid Login";
+        }
+    }
+    
+   if ($Check == 'student') {
+        
+        $sql = mysql_query("SELECT COUNT(*) FROM StudentLogin SL WHERE Student_ID = '$User' AND Student_PW = '$Pass'");
+        $info = mysql_fetch_assoc($sql);
+        $ver = $info['COUNT(*)'];
+        
+        if ($ver == 1) {
+            session_start();
+            $_SESSION['UserST'] = $User;
+            echo "student connected";
+        }
+        else {
+            header ("Location: http://web.njit.edu/~ovl2/CS490/Front/login.html");
+            echo "Invalid Login";
+        } 
+    } 
+
+    if ($Check == 'Login') {
+        $reg = "INSERT INTO StudentLogin (Student_ID, Student_PW) VALUES ('$User', '$Pass')";
+        $exec = mysql_query($reg, $con);
+        header ("Location: http://web.njit.edu/~ovl2/CS490/Front/login.html");
+    } 
 ?>
