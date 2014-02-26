616aece22daf738a3ec8f8bf88c31e10dcd833b9
OrazioLentini <oraziio@hotmail.com>
2014-02-26 13:43:54 -0500

    Update connect.php

--- Front/connect.php
+++ Front/connect.php
@@ -1,14 +1,18 @@
 <?php
-        $Username=$_POST['Username'];
-        $Password=$_POST['Password'];
+	$Username=$_POST['name'];
+	$Password=$_POST['word'];
+	$Check = $_POST['check'];
+	
+	$fields = array('Username' => $Username, 'Password' => $Password, 'Check' => $Check);
+	$send = json_encode($fields);
+	
+	$crl = curl_init();
+	//curl_setopt($crl, CURLOPT_URL, "http://web.njit.edu/~ovl2/CS490/Middle/middlelogin.php");
+	curl_setopt($crl, CURLOPT_URL, "http://web.njit.edu/~rab25/CS490/Middle/middlelogin.php");
+	curl_setopt($crl, CURLOPT_POST, 1);
+	curl_setopt($crl, CURLOPT_POSTFIELDS, $send);
+	curl_setopt($crl, CURLOPT_FOLLOWLOCATION, 1);
 
-        $crl =curl_init();
-        curl_setopt($crl, CURLOPT_URL,
-        "http://web.njit.edu/~rab25/CS490/Middle/middlelogin.php");
-        curl_setopt($crl, CURLOPT_POST, 1);
-        curl_setopt($crl, CURLOPT_POSTFIELDS, "Username=$Username&Password=$Password");
-        curl_setopt($crl, CURLOPT_FOLLOWLOCATION, 1);
-        $crlm = curl_exec($crl);
-        curl_close($crl);
-
+	$c = curl_exec($crl);
+	curl_close($crl); 
 ?>
