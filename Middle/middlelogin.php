f1929ed90d3fca6d6ef005498ff4279d64e560aa
OrazioLentini <oraziio@hotmail.com>
2014-02-26 13:43:00 -0500

    Update middlelogin.php

--- Middle/middlelogin.php
+++ Middle/middlelogin.php
@@ -1,16 +1,14 @@
 <?php
-    $Username = $_POST['Username'];
-    $Password = $_POST['Password'];
-    $Check = $_POST['Check'];
-        
+    $request = file_get_contents('php://input');
+    
     $loginDB = curl_init();
-    curl_setopt($loginDB, CURLOPT_URL, "http://web.njit.edu/~ovl2/CS490/Back/login.php");
+    curl_setopt($loginDB, CURLOPT_URL, "http://web.njit.edu/~ovl2/CS490/Back/backlogin.php");
     curl_setopt($logibDB, CURLOPT_POST, 1);
-    curl_setopt($loginDB, CURLOPT_POSTFIELDS, "Username=$Username&Password=$Password&Check=$Check");
+    curl_setopt($loginDB, CURLOPT_POSTFIELDS, $request);
     curl_setopt($loginDB, CURLOPT_FOLLOWLOCATION, 1);
     
     $outLoginDB = curl_exec($loginDB);
-    curl_close($loginDB); 
+    curl_close($loginDB);  
 ?>
 
  
