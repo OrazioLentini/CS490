5d1b205236258731f9bd8e607195a54a284bab7b
OrazioLentini <oraziio@hotmail.com>
2014-02-27 16:14:05 -0500

    Update addQuestions.php

--- Back/addQuestions.php
+++ Back/addQuestions.php
@@ -10,25 +10,27 @@
     $recieve = json_decode($request);
     
     $Question = $recieve->Question;
-    $Opt1 = -> $recieve->Opt1;
-    $Opt2 = -> $recieve->Opt2;
-    $Opt3 = -> $recieve->Opt3;
-    $Opt4 = -> $recieve->Opt4;
+    $Opt1 = $recieve->Opt1;
+    $Opt2 = $recieve->Opt2;
+    $Opt3 = $recieve->Opt3;
+    $Opt4 = $recieve->Opt4;
     $Answer = $recieve->Answer;
     $Type = $recieve->Type;
     
     if ($Type == 'MC') {
-        $createMC = "INSERT INTO MultipleChoice (Question, Opt1, Opt2, Opt3, Opt4, Answer) VALUES ('$Question', '$Opt1', '$Opt2', 'Opt3', 'Opt4', '$Answer')";
-        $exec = mysql_query($createMC, $con);
+       $createMC = "INSERT INTO MultipleChoice (Question, Opt1, Opt2, Opt3, Opt4, Answer) VALUES ('$Question', '$Opt1', '$Opt2', 'Opt3', 'Opt4', '$Answer')";
+       $exec = mysql_query($createMC, $con);
     }
     
     if ($Type == 'TF') {
+        echo 'TF';
         $createMC = "INSERT INTO TrueFalse (Question, Opt1, Opt2, Answer) VALUES ('$Question', '$Opt1', '$Opt2', '$Answer')";
         $exec1 = mysql_query($createTF, $con);
     }
     
     if ($Type == 'OE') {
+        echo 'OE';
         $createMC = "INSERT INTO OpenEnded (Question) VALUES ('$Question')";
         $exec2 = mysql_query($createOE, $con);
-    }
+    } 
 ?>
