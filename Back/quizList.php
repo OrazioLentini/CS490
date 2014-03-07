b71b6308f58e9d169a3d7daaf3e55959cf81730b
OrazioLentini <oraziio@hotmail.com>
2014-03-07 17:57:51 -0500

    3/7/2014

--- Back/quizList.php
+++ Back/quizList.php
@@ -1,4 +1,9 @@
 <?php
+    session_start;
+    include('sheader.php');
+?>
+
+<?php
     $con = mysql_connect("sql.njit.edu","ovl2_proj","cs490");
     if(!$con) {
         die('Could not connect: ' . mysql_error());
@@ -15,15 +20,16 @@
     }
     
     $list = json_encode(array('Quizzes' => $listQuiz));
-    echo $list;
+    //echo $list;
     
-    /*$crl = curl_init();
+    $crl = curl_init();
     //curl_setopt($crl, CURLOPT_URL, "http://web.njit.edu/~ovl2/CS490/Front/login.php");
-    curl_setopt($crl, CURLOPT_URL, "http://web.njit.edu/~ic35/CS490/login/student/student.php");
+    //curl_setopt($crl, CURLOPT_URL, "http://web.njit.edu/~ic35/CS490/login/student/quizList.php");
+    curl_setopt($crl, CURLOPT_URL, "http://web.njit.edu/~ovl2/CS490/login/student/quizList.php");
     curl_setopt($crl, CURLOPT_POST, 1);
-    curl_setopt($crl, CURLOPT_POSTFIELDS, $Qus);
+    curl_setopt($crl, CURLOPT_POSTFIELDS, $list);
     curl_setopt($crl, CURLOPT_FOLLOWLOCATION, 1);
     
     $outputDB = curl_exec($crl);
-    curl_close($crl);*/
+    curl_close($crl);
 ?>
