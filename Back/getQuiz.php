91dd51293352234554e6a05edf628bbad62ed07a
OrazioLentini <oraziio@hotmail.com>
2014-03-29 11:43:37 -0400

    Update getQuiz.php

--- Back/getQuiz.php
+++ Back/getQuiz.php
@@ -1,4 +1,8 @@
 <?php
+    session_start;
+    include('sheader.php');
+?>
+<?php
     $con = mysql_connect("sql.njit.edu","ovl2_proj","cs490");
     if(!$con) {
         die('Could not connect: ' . mysql_error());
@@ -11,10 +15,9 @@
     
     $Name = $Quiz->QuizName;
     
-    
     $quizMC = mysql_query("SELECT Question, Opt1, Opt2, Opt3, Opt4, QuestionNum FROM `$Name` WHERE `Opt3` != '' AND `Opt4` != ''");
     $quizTF = mysql_query("SELECT Question, Opt1, Opt2, QuestionNum FROM `$Name` WHERE `Opt1` = 'True' AND `Opt2` = 'False'");
-    $quizOE = mysql_query("SELECT Question FROM `Quiz1` WHERE `Answer` IS NULL");
+    $quizOE = mysql_query("SELECT Question FROM `$Name` WHERE Answer != 'A' AND Answer != 'B' AND Answer != 'C' AND Answer != 'D' AND Answer != 'True' AND Answer != 'False'");
     
     $MCquestions = array();
     $TFquestions = array();
@@ -33,17 +36,16 @@
     }
     
     
-    $Quiz = json_encode(array('MultipleChoice' => $MCquestions, 'TrueFalse' => $TFquestions, 'OpenEnded' => $OEquestions));
+    $Quiz = json_encode(array('QuizName' => $Name, 'MultipleChoice' => $MCquestions, 'TrueFalse' => $TFquestions, 'OpenEnded' => $OEquestions));
     //echo $Quiz;
     
     $crl = curl_init();
-    //curl_setopt($crl, CURLOPT_URL, "http://web.njit.edu/~ovl2/CS490/Front/login.php");
+    //curl_setopt($crl, CURLOPT_URL, "http://web.njit.edu/~ovl2/CS490/login/student/takeQuiz.php");
     curl_setopt($crl, CURLOPT_URL, "http://web.njit.edu/~ic35/CS490/login/student/takeQuiz.php");
     curl_setopt($crl, CURLOPT_POST, 1);
-    curl_setopt($crl, CURLOPT_POSTFIELDS, $Qus);
+    curl_setopt($crl, CURLOPT_POSTFIELDS, $Quiz);
     curl_setopt($crl, CURLOPT_FOLLOWLOCATION, 1);
     
     $outputDB = curl_exec($crl);
-    curl_close($crl);
+    curl_close($crl); 
 ?>
-    
