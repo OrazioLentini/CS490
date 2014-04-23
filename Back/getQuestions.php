f14720b41852be01d696e7c7c915e1dac9ede066
OrazioLentini <oraziio@hotmail.com>
2014-04-23 12:57:21 -0400

    Update getQuestions.php

--- Back/getQuestions.php
+++ Back/getQuestions.php
@@ -9,6 +9,10 @@
     $sqlMC = mysql_query("SELECT Question, QuestionNum  FROM MultipleChoice");
     $sqlTF = mysql_query("SELECT Question, QuestionNum FROM TrueFalse");
     $sqlOE = mysql_query("SELECT Question, QuestionNum FROM OpenEnded");
+    
+    $sqlEasy = mysql_query("SELECT Question, QuestionNum FROM MultipleChoice WHERE Difficulty='Easy' UNION SELECT Question, QuestionNum FROM TrueFalse WHERE Difficulty='Easy' UNION SELECT Question, QuestionNum FROM OpenEnded WHERE Difficulty='Easy'");
+    $sqlMedium = mysql_query("SELECT Question, QuestionNum FROM MultipleChoice WHERE Difficulty='Medium' UNION SELECT Question, QuestionNum FROM TrueFalse WHERE Difficulty='Medium' UNION SELECT Question, QuestionNum FROM OpenEnded WHERE Difficulty='Medium'");
+    $sqlHard = mysql_query("SELECT Question, QuestionNum FROM MultipleChoice WHERE Difficulty='Hard' UNION SELECT Question, QuestionNum FROM TrueFalse WHERE Difficulty='Hard' UNION SELECT Question, QuestionNum FROM OpenEnded WHERE Difficulty='Hard'");
 
     $MCquestions = array();
     $TFquestions = array();
@@ -26,17 +30,35 @@
             $OEquestions[] = $oe;
     }
     
+    $Easyquestions = array();
+    $Mediumquestions = array();
+    $Hardquestions = array();
+    
+    while($e = mysql_fetch_assoc($sqlEasy)) {
+            $Easyquestions[] = $e;
+    }
+    
+    while($m = mysql_fetch_assoc($sqlMedium)) {
+            $Mediumquestions[] = $m;
+    }
+    
+    while($h = mysql_fetch_assoc($sqlHard)) {
+            $Hardquestions[] = $h;
+    }
+    
     //echo json_encode($MCquestions);
     //echo json_encode($TFquestions);
     
-    $Qus = json_encode(array('MultipleChoice' => $MCquestions, 'TrueFalse' => $TFquestions, 'OpenEnded' => $OEquestions));
+    $Qus = json_encode(array('MultipleChoice' => $MCquestions, 'TrueFalse' => $TFquestions, 'OpenEnded' => $OEquestions, 'Easy' => $Easyquestions, 'Medium' => $Mediumquestions, 'Hard' => $Hardquestions));
+    //echo $Qus;
     
     $crl = curl_init();
-    curl_setopt($crl, CURLOPT_URL, "http://web.njit.edu/~ovl2/CS490/Front/getQuestions.php");
+    //curl_setopt($crl, CURLOPT_URL, "http://web.njit.edu/~rab25/CS490/Middle/getQuestions.php");
+    curl_setopt($crl, CURLOPT_URL, "http://web.njit.edu/~ic35/CS490/login/teacher/quiz.php");
     curl_setopt($crl, CURLOPT_POST, 1);
     curl_setopt($crl, CURLOPT_POSTFIELDS, $Qus);
     curl_setopt($crl, CURLOPT_FOLLOWLOCATION, 1);
     
     $outputDB = curl_exec($crl);
-    curl_close($crl);
+    curl_close($crl); 
 ?>
