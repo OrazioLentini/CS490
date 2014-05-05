2140d9f7de173f95f0aa84f1a6a863195bb067f1
OrazioLentini <oraziio@hotmail.com>
2014-05-05 13:48:44 -0400

    Create points.php

---
+++ Front/points.php
@@ -0,0 +1,20 @@
+<?php 
+session_start();
+include('theader.php');
+
+
+    $P = $_POST['points'];
+    $name = $_POST['name'];
+
+    $fields = json_encode(array('QuizName' => $name, 'Points' => $P));
+    //echo $fields;
+    
+    $crl = curl_init();
+    curl_setopt($crl, CURLOPT_URL, "http://web.njit.edu/~ovl2/CS490/Back/addPoints.php");
+    curl_setopt($crl, CURLOPT_POST, 1);
+    curl_setopt($crl, CURLOPT_POSTFIELDS, $fields);
+    curl_setopt($crl, CURLOPT_FOLLOWLOCATION, 1);
+
+    $c = curl_exec($crl);
+    curl_close($crl); 
+?>
